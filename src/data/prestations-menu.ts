7b0b46f64b7ba3d7b55341d9adb652a68558653a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-04-03 13:07:29 +0200

    dev: prestations

---
+++ src/data/prestations-menu.ts
@@ -0,0 +1,19 @@
+import type { MenuItem } from "../types/menu";
+
+export const prestationsMenuItems: MenuItem[] = [
+  { label: "Conseil stratégique", href: "prestations/conseil-strategique" },
+  {
+    label: "Développement logiciel",
+    href: "prestations/developpement-logiciel",
+  },
+  { label: "Formation", href: "prestations/formation" },
+  { label: "Accessibilité numérique", href: "prestations/accessibilite" },
+  {
+    label: "Architecture technique",
+    href: "prestations/architecture-technique",
+  },
+  {
+    label: "Design UX/UI",
+    href: "prestations/design-ux-ui",
+  },
+];
