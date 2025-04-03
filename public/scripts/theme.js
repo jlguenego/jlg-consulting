4e1ea49acd16c45b05259e476bf635a8e0bb36ab
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-04-03 17:03:55 +0200

    dev: dark sync with os

--- public/scripts/theme.js
+++ public/scripts/theme.js
@@ -6,11 +6,21 @@ function applyTheme() {
 }
 
 function toggleTheme() {
-  console.log("toggleTheme");
   const html = document.documentElement;
   const isDark = html.classList.contains("dark");
   html.classList.toggle("dark", !isDark);
   localStorage.setItem("theme", isDark ? "light" : "dark");
 }
 
+// Applique le thème au chargement
 applyTheme();
+
+// Met à jour le thème si les préférences système changent
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (e) => {
+    console.log("change theme");
+
+    document.documentElement.classList.toggle("dark", e.matches);
+    localStorage.setItem("theme", e.matches ? "dark" : "light");
+  });
