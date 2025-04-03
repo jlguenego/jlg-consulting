46b99600028071e868ca31024154f0d9a72af1cf
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-04-03 15:14:06 +0200

    dev: added redirection

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -5,3 +5,10 @@ export function getLocaleFromPathname(pathname: string): Locale {
   if (pathname.includes("/fr/")) return "fr";
   return "fr"; // fallback par défaut
 }
+
+export function getLocaleFromCache(): Locale {
+  const locale = localStorage.getItem("locale") as Locale;
+  if (locale === "en" || locale === "fr") return locale;
+  localStorage.setItem("locale", "fr");
+  return "fr"; // fallback par défaut
+}
