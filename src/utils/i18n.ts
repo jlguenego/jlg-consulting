e74e6a3e39aeb9bae1f187b6da86457d004a424b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-04-03 14:10:43 +0200

    dev: type locale

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,4 +1,6 @@
-export function getLocaleFromPathname(pathname: string): string {
+type Locale = "fr" | "en";
+
+export function getLocaleFromPathname(pathname: string): Locale {
   if (pathname.includes("/en/")) return "en";
   if (pathname.includes("/fr/")) return "fr";
   return "fr"; // fallback par défaut
