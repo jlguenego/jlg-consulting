b52d8c2c432a8d352002382082a02a980ec5ac58
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-04-06 15:13:19 +0200

    dev: added markdown

--- src/components/MarkdownComponents.tsx
+++ src/components/MarkdownComponents.tsx
@@ -1,6 +1,24 @@
 export default {
-  h1: (props: any) => <h1 className="my-6 text-3xl font-bold" {...props} />,
-  h2: (props: any) => <h2 className="my-4 text-2xl font-semibold" {...props} />,
-  h3: (props: any) => <h3 className="my-2 text-xl font-medium" {...props} />,
+  h1: (props: any) => (
+    <h1
+      className="font-heading my-8 text-center text-3xl font-bold text-green-700"
+      {...props}
+    />
+  ),
+  h2: (props: any) => (
+    <h2
+      className="font-heading mb-4 text-2xl font-semibold text-green-700"
+      {...props}
+    />
+  ),
+  h3: (props: any) => (
+    <h3
+      className="font-heading mb-4 text-xl font-medium text-green-700"
+      {...props}
+    />
+  ),
+  hr: (props: any) => <hr className="my-4 border-b" {...props} />,
+  ul: (props: any) => <ul className="mb-4" {...props} />,
+  li: (props: any) => <li className="my-2 ml-8 list-disc" {...props} />,
   // Ajoute d'autres composants si besoin (p, a, ul, li, etc.)
 };
