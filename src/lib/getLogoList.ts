f9691ffc557da98c5f31809042d7ad5620818f76
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-04-08 09:30:48 +0200

    dev: using ImageLoader

--- src/lib/getLogoList.ts
+++ src/lib/getLogoList.ts
@@ -2,16 +2,23 @@
 import fs from "fs";
 import path from "path";
 
-export function getLogoList(): string[] {
+export function getLogoList(): { img: string; smallImg: string }[] {
   const logosDir = path.resolve("public/logos");
   if (!fs.existsSync(logosDir)) return [];
 
-  const result = fs
+  const logos = fs
     .readdirSync(logosDir)
     .filter((file) => /\.(png|jpe?g|svg)$/i.test(file))
     .filter((file) => {
       return /small/.test(file) === false;
     });
-  console.log("result: ", result);
+  const result = [];
+  for (const logo of logos) {
+    result.push({
+      img: logo,
+      smallImg: logo.replace(/(\.png|\.jpg|\.jpeg|\.svg)/i, "-small$1"),
+    });
+  }
+
   return result;
 }
